abc7d2b95c7fbc1b6190281969a67e5d41d99bce
杨天帅 <myword@126.com>
2023-04-27 08:29:29 +0800

    增加文件映射

---
+++ Result/FileResult.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System.IO;
+using System.Text;
+
+namespace System.HttpProxy
+{
+    /// <summary>
+    /// 文件转换器
+    /// 创建人：YTS
+    /// 创建时间：2023-4-4
+    /// </summary>
+    public class FileResult : ActionResult
+    {
+        private object _value;
+
+        public FileResult(byte[] buffer, string file_name, string file_ext)
+        {            
+            this.ContentType = MimeMapping.GetMimeMapping("." + file_ext);
+        }
+
+      
+        public override string ToResponse()
+        {
+            
+            return string.Empty;
+        }
+
+    }
+
+}
